52687699f2c2c831b466778be4c994cb87597fb2
AjayPS1998 <106143403+AjayPS1998@users.noreply.github.com>
2023-03-14 20:29:51 +0530

    only testinsdng st project

---
+++ src/homePage.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import './homepage.styles.scss'
+
+const HomePage = () =>(
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Ajay</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Parvathy</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">DeviPriya</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Pranav</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+);
+
+export default HomePage;
